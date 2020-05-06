88f945c41a95a9addd03d5c3eed75f3360d3ed37
gitleschr <chsiagianles@gmail.com>
2020-05-06 22:16:26 +0700

    initial commit

---
+++ app.js
@@ -0,0 +1,15 @@
+new Vue({
+    el:'#app',
+    data: {
+        nama: 'Charles',
+        website : 'https://www.mokapos.com/',
+        websiteTag: '<a href="https://www.qiscus.com/">kuro'
+    },
+    methods:{
+        openInNewTab: function(url) {
+            var win = window.open(url, '_blank');
+            win.focus()
+        }
+    }
+
+})
